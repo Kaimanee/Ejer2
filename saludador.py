854b453f5edd92168cc54b2f3befd0e53c6bf316
corpasx <damian.corpas@itgranada.com>
2025-05-12 12:17:20 +0200

    Añade saludo Damian2

--- saludador.py
+++ saludador.py
@@ -2,6 +2,6 @@ print('Bienvenid@ al saludador grupal')
 
 print('Saludo Damian 1')
 
-print('Hola, soy Damian espero q este todo bien')
+print('Hola, soy Damian espero q este todo bien y correcto')
 
 input()
